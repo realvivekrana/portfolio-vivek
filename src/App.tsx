a89b4940aabbc3e35d0b72bf09b240a7457b66f0
Vivek Kumar Rana <vivekkumarrana5567@gmail.com>
2026-03-07 17:03:20 +0530

    Fix GitHub Pages deployment - Add basename to BrowserRouter and 404.html for routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/-Vivek-Rana-MERN-Full-Stack-Developer-Portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
